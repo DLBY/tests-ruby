10b2f2ba9561ce6a1a9e78d38dab1781c6573420
DLBY <david.lobry96@gmail.com>
2020-10-05 18:02:58 +0200

    adding 06.rb

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,5 +1,10 @@
 def translate(word)
-  word.join('ay')
-end
+  word.split.map { |x| 
+  if (x[0] =~ /[aeiouy]/) then x + "ay"
+  elsif (x[0] !=~ /[aeiouy]/) && (x[1] =~ /[aeiouy]/) then x[1..x.length] + x[0] + "ay"
+  elsif (x[0] !=~ /[aeiouy]/) && (x[1] !=~ /[aeiouy]/) then x[2..x.length] + x[0] + x[1] + "ay"
+  elsif (x[0] !=~ /[aeiouy]/) && (x[1] !=~ /[aeiouy]/ && (x[2] !=~ /[aeiouy]/)  then x[2..x.length] + x[0] + x[1] + x[2] + "ay"
 
-#You can use the << method, which is an alias for concat.
+  end 
+}.join(' ')
+end
